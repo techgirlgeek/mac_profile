0ce26bbd381a9a486d94f91981fda41901ac2946
techgirlgeek <karyn@techgirlgeek.com>
2017-12-22 15:35:17 -0700

    Add git aliases

--- zsh_custom/aliases.zsh
+++ zsh_custom/aliases.zsh
@@ -4,3 +4,5 @@ alias workshuttle="source /Users/kcassio/.zshrc;sshuttle -r kcassio@sea1l3vipcj0
 alias homeshuttle='sshuttle -r kcassio@sea1l3vipcj01.davita.corp 10.9.0.0/16 10.10.0.0/16'
 alias ddig="dig drupal.slack.com | egrep "^drupal" | awk '{print \$5}'"
 alias sourcez="source /Users/kcassio/.zshrc"
+alias gitprunedry="git remote prune origin --dry-run"
+alias gitprune="git remote prune origin"
