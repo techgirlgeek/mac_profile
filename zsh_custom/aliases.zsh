4d902152d2d5183a9f8a779fba9d9b938f7e9511
techgirlgeek <karyn@techgirlgeek.com>
2018-01-12 12:08:25 -0700

    Update zsh aliases

--- zsh_custom/aliases.zsh
+++ zsh_custom/aliases.zsh
@@ -1,7 +1,7 @@
 alias lstr='ls -ltrh'
 alias ll='ls -lh'
-alias workshuttle="source /Users/kcassio/.zshrc;sshuttle -r kcassio@sea1l3vipcj01.davita.corp `dig drupal.slack.com | egrep "^drupal" | awk '{print \$5}'`/32 10.9.0.0/16 10.10.0.0/16"
-alias homeshuttle='sshuttle -r kcassio@sea1l3vipcj01.davita.corp 10.9.0.0/16 10.10.0.0/16'
+alias workshuttle="source /Users/kcassio/.zshrc;sshuttle -r kcassio@sea1l3vipcj01.davita.corp `dig drupal.slack.com | egrep "^drupal" | awk '{print \$5}'`/32 10.9.0.0/16 10.10.0.0/16 10.12.0.0/16"
+alias homeshuttle='sshuttle -r kcassio@sea1l3vipcj01.davita.corp 10.9.0.0/16 10.10.0.0/16 10.12.0.0/16'
 alias ddig="dig drupal.slack.com | egrep "^drupal" | awk '{print \$5}'"
 alias sourcez="source /Users/kcassio/.zshrc"
 alias gitprunedry="git remote prune origin --dry-run"
